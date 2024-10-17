380be010d318f10da3a767dfcef9cb3296bb6df0
rishabh <rishabh.srivastava@phot.ai>
2024-10-17 08:25:24 +0000

    updated setup.sh

--- setup.sh
+++ setup.sh
@@ -2,3 +2,5 @@ python -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
 mkdir output
+git config --global user.email rishabh.srivastava@phot.ai
+git config --global user.name rishabh
