f9881117b78b48818b551d2d1692f3ed2c95360c
rishabh <rishabh.srivastava@phot.ai>
2024-10-23 06:46:38 +0000

    added textual inversion and seed

--- src/diffuser.py
+++ src/diffuser.py
@@ -1,5 +1,5 @@
-from diffusers import StableDiffusionXLPipeline
-from diffusers import DPMSolverMultistepScheduler, EulerDiscreteScheduler
+from diffusers import StableDiffusionXLPipeline, UNet2DConditionModel
+from diffusers import DPMSolverMultistepScheduler, EulerDiscreteScheduler, LCMScheduler
 from safetensors.torch import load_file
 from diffusers import AutoencoderKL
 
@@ -10,23 +10,6 @@ import torch
 
 
 
-
-def inject_embeddings(pipe, embeddings):
-    # Add new tokens and their embeddings to the tokenizer and text encoder
-    tokenizer = pipe.tokenizer
-    text_encoder = pipe.text_encoder
-
-    for emb_name, embedding in embeddings.items():
-        # Add the embedding as a new token
-        tokenizer.add_tokens([emb_name])
-        token_id = tokenizer.convert_tokens_to_ids(emb_name)
-
-        # Resize the token embeddings to accommodate the new token
-        text_encoder.resize_token_embeddings(len(tokenizer))
-        
-        # Set the embedding for the new token
-        with torch.no_grad():
-            text_encoder.get_input_embeddings().weight[token_id] = embedding
 
 def  create_pipe():
 
@@ -45,20 +28,18 @@ def  create_pipe():
         "DPMPP_2M_SDE_Lu": (DPMSolverMultistepScheduler, {"use_lu_lambdas": True, "algorithm_type": "sde-dpmsolver++"}),
         "DPMPP_2M_SDE_Stable": (DPMSolverMultistepScheduler, {"algorithm_type": "sde-dpmsolver++", "euler_at_final": True}),
     }
-    scheduler = schedulers["DPMPP_2M_SDE_Lu"][0].from_pretrained(
+    scheduler = schedulers["DPMPP_2M_SDE_Stable"][0].from_pretrained(
                 "SG161222/RealVisXL_V4.0_Lightning",
                 subfolder="scheduler",
-                **schedulers["DPMPP_2M_SDE_Lu"][1],
+                **schedulers["DPMPP_2M_SDE_Stable"][1],
             )
     # Inject embeddings into the text encoder
 
 
     # seed 
     seed =1
-    generator = torch.Generator(device='cuda').manual_seed(seed)
 
-    vae = AutoencoderKL.from_pretrained("madebyollin/sdxl-vae-fp16-fix", torch_dtype=torch.float16)
-
+    vae = AutoencoderKL.from_pretrained("stabilityai/sdxl-vae", torch_dtype=torch.float16)
     model_loading_time = time()
     pipe = StableDiffusionXLPipeline.from_pretrained("SG161222/RealVisXL_V4.0_Lightning",
                                                      vae=vae,
@@ -66,6 +47,7 @@ def  create_pipe():
                                                         variant = "fp16",
                                                         requires_grad=False)
     pipe.scheduler = scheduler
+    # pipe.scheduler = LCMScheduler.from_config(pipe.scheduler.config)
     pipe.to("cuda")
 
     # pipe.enable_xformers_memory_efficient_attention() # Increases inference time
@@ -74,17 +56,25 @@ def  create_pipe():
     return pipe
 
 
-def run_pipe(pipe,prompt):
+def run_pipe(pipe,prompt, embeddings):
+    negative_prompt = "(octane render, render, drawing, anime, bad photo, bad photography:1.3),\
+        (worst quality, low quality, blurry:1.2),\
+            (bad teeth, deformed teeth, deformed lips),\
+                (bad anatomy, bad proportions:1.1),\
+                    (deformed iris:1.2, deformed pupils:1.2), (deformed eyes:1.2, bad eyes:1.2), \
+                        (deformed face, ugly face, bad face), (deformed hands, bad hands, fused fingers), morbid, mutilated, mutation, disfigured"
     with torch.no_grad():
 
         images = pipe(prompt,
                         guidance_scale=1,
-                        negative_prompt="(octane render, render, drawing, anime, bad photo, bad photography:1.3), (worst quality, low quality, blurry:1.2), (bad teeth, deformed teeth, deformed lips), (bad anatomy, bad proportions:1.1), (deformed iris, deformed pupils), (deformed eyes, bad eyes), (deformed face, ugly face, bad face), (deformed hands, bad hands, fused fingers), morbid, mutilated, mutation, disfigured",
+                        negative_prompt= negative_prompt,
                         num_inference_steps=15,
                         strength=1,
                         width=1024,
                         height=1024,
-                        num_images_per_prompt=4,
+                        num_images_per_prompt=10,
+                        generator = torch.Generator(device='cuda').manual_seed(1),
+                        # negative_prompt_embeds = embeddings 
                       ).images 
     return images 
 
@@ -97,18 +87,17 @@ with open('../prompts.json', 'r') as f:
 
 pipe =create_pipe()
 
-# # Load all embeddings
-# embeddings_dir = "../embeddings"
+# Load all embeddings
+embeddings_dir = "../embeddings"
 
-# embeddings = {}
-# for emb_file in os.listdir(embeddings_dir):
-#     if not emb_file.endswith('.pt'):
-#         continue
-#     embedding_path = os.path.join(embeddings_dir, emb_file)
-#     embeddings[emb_file] = torch.load(embedding_path, map_location='cuda')
+embeddings = []
+for emb_file in os.listdir(embeddings_dir):
+    if not emb_file.endswith('.pt'):
+        continue
+    embedding_path = os.path.join(embeddings_dir, emb_file)
+    embeddings.append(embedding_path)
 
-# inject_embeddings(pipe, embeddings)
-
+pipe.load_textual_inversion(embeddings)
 
 average_time =0
 # Running Inference
@@ -123,7 +112,7 @@ for prompt_index in range(len(prompts)):
     print(prompt)
     st_time = time()
 
-    images = run_pipe(pipe,prompt)
+    images = run_pipe(pipe,prompt, embeddings)
         
     print(f'For prompt {prompt_index} time taken is:', time()-st_time)
     average_time+=time()-st_time
